015dcff940d3c24ff566f28b7e17763b8e15872c
Ryan Milstead <ryanmilstead1@gmail.com>
2021-05-12 10:26:22 -0400

    Update native_authentication to allow scopes

--- lib/nylas/native_authentication.rb
+++ lib/nylas/native_authentication.rb
@@ -1,28 +1,36 @@
+# frozen_string_literal: true
+
 module Nylas
   # Authenticate your application using the native interface
   # @see https://docs.nylas.com/reference#native-authentication-1
   class NativeAuthentication
     attr_accessor :api
+
     def initialize(api:)
       self.api = api
     end
 
-    def authenticate(name:, email_address:, provider:, settings:, reauth_account_id: nil)
+    def authenticate(name:, email_address:, provider:, settings:, reauth_account_id: nil,
+                     scopes: nil)
+      scopes ||= %w[email calendar contacts]
+      scopes = scopes.join(",") unless scopes.is_a?(String)
       code = retrieve_code(name: name, email_address: email_address, provider: provider,
-                           settings: settings, reauth_account_id: reauth_account_id)
+                           settings: settings, reauth_account_id: reauth_account_id, scopes: scopes)
 
       exchange_code_for_access_token(code)
     end
 
-    private def retrieve_code(name:, email_address:, provider:, settings:, reauth_account_id:)
+    private
+
+    def retrieve_code(name:, email_address:, provider:, settings:, reauth_account_id:, scopes:)
       payload = { client_id: api.client.app_id, name: name, email_address: email_address,
-                  provider: provider, settings: settings }
+                  provider: provider, settings: settings, scopes: scopes }
       payload[:reauth_account_id] = reauth_account_id
       response = api.execute(method: :post, path: "/connect/authorize", payload: JSON.dump(payload))
       response[:code]
     end
 
-    private def exchange_code_for_access_token(code)
+    def exchange_code_for_access_token(code)
       payload = { client_id: api.client.app_id, client_secret: api.client.app_secret, code: code }
       response = api.execute(method: :post, path: "/connect/token", payload: JSON.dump(payload))
       response[:access_token]
