aa9535e5fdad786120d41f26729bc5e6b8fbdc9b
Ryan <ryanmilstead1@gmail.com>
2021-05-12 10:40:14 -0400

    Update api with more recent changes

--- lib/nylas/api.rb
+++ lib/nylas/api.rb
@@ -1,9 +1,12 @@
+# frozen_string_literal: true
+
 module Nylas
   # Methods to retrieve data from the Nylas API as Ruby objects
   class API
     attr_accessor :client
+
     extend Forwardable
-    def_delegators :client, :execute, :get, :post, :put, :delete, :app_id
+    def_delegators :client, :execute, :get, :post, :put, :delete, :app_id, :api_server
 
     include Logging
 
@@ -15,25 +18,58 @@ module Nylas
     #                            you're using a self-hosted Nylas instance.
     # @param service_domain [String] (Optional) Host you are authenticating OAuth against.
     # @return [Nylas::API]
-    # rubocop:disable Metrics/ParameterLists
     def initialize(client: nil, app_id: nil, app_secret: nil, access_token: nil,
                    api_server: "https://api.nylas.com", service_domain: "api.nylas.com")
       self.client = client || HttpClient.new(app_id: app_id, app_secret: app_secret,
                                              access_token: access_token, api_server: api_server,
                                              service_domain: service_domain)
     end
-    # rubocop:enable Metrics/ParameterLists
 
     # @return [String] A Nylas access token for that particular user.
-    def authenticate(name:, email_address:, provider:, settings:, reauth_account_id: nil)
-      NativeAuthentication.new(api: self).authenticate(name: name, email_address: email_address,
-                                                       provider: provider, settings: settings,
-                                                       reauth_account_id: reauth_account_id)
+    def authenticate(name:, email_address:, provider:, settings:, reauth_account_id: nil, scopes: nil)
+      NativeAuthentication.new(api: self).authenticate(
+        name: name,
+        email_address: email_address,
+        provider: provider,
+        settings: settings,
+        reauth_account_id: reauth_account_id,
+        scopes: scopes
+      )
+    end
+
+    def authentication_url(redirect_uri:, scopes:, response_type: "code", login_hint: nil, state: nil)
+      params = {
+        client_id: app_id,
+        redirect_uri: redirect_uri,
+        response_type: response_type,
+        login_hint: login_hint
+      }
+      params[:state] = state if state
+      params[:scopes] = scopes.join(",") if scopes
+
+      "#{api_server}/oauth/authorize?#{URI.encode_www_form(params)}"
+    end
+
+    def exchange_code_for_token(code)
+      data = {
+        "client_id" => app_id,
+        "client_secret" => client.app_secret,
+        "grant_type" => "authorization_code",
+        "code" => code
+      }
+
+      response_json = execute(method: :post, path: "/oauth/token", payload: data)
+      response_json[:access_token]
     end
 
     # @return [Collection<Contact>] A queryable collection of Contacts
     def contacts
       @contacts ||= Collection.new(model: Contact, api: self)
+    end
+
+    # @return [Collection<ContactGroup>] A queryable collection of Contact Groups
+    def contact_groups
+      @contact_groups ||= Collection.new(model: ContactGroup, api: self)
     end
 
     # @return [CurrentAccount] The account details for whomevers access token is set
@@ -94,6 +130,14 @@ module Nylas
       response.code == 200 && response.empty?
     end
 
+    # Returns list of IP addresses
+    # @return [Hash]
+    # hash has keys of :updated_at (unix timestamp) and :ip_addresses (array of strings)
+    def ip_addresses
+      path = "/a/#{app_id}/ip_addresses"
+      client.as(client.app_secret).get(path: path)
+    end
+
     # @param message [Hash, String, #send!]
     # @return [Message] The resulting message
     def send!(message)
@@ -119,8 +163,20 @@ module Nylas
       @webhooks ||= Collection.new(model: Webhook, api: as(client.app_secret))
     end
 
-    private def prevent_calling_if_missing_access_token(method_name)
+    def free_busy(emails:, start_time:, end_time:)
+      FreeBusyCollection.new(
+        api: self,
+        emails: emails,
+        start_time: start_time.to_i,
+        end_time: end_time.to_i
+      )
+    end
+
+    private
+
+    def prevent_calling_if_missing_access_token(method_name)
       return if client.access_token && !client.access_token.empty?
+
       raise NoAuthToken, method_name
     end
   end
